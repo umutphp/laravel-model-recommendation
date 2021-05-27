45f6e9458756621533b4bab5d188f250eaa399ce
Umut Işık <umutphp@gmail.com>
2021-05-27 20:30:58 +0300

    Add fillable fields to model

--- src/RecommendationsModel.php
+++ src/RecommendationsModel.php
@@ -2,10 +2,17 @@
 
 namespace Umutphp\LaravelModelRecommendation;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class RecommendationsModel extends Model
 {
     protected $table = 'laravel_model_recommendation_table';
+
+    protected $fillable = [
+        'source_type',
+        'source_id',
+        'target_type',
+        'target_id',
+        'order_column'
+    ];
 }
