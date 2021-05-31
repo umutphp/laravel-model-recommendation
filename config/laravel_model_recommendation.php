7aa896b72340ec9eddc8062ec49b0c93c1f1e072
Umut Işık <umutphp@gmail.com>
2021-05-31 23:03:47 +0300

    Fix linter bug

--- config/laravel_model_recommendation.php
+++ config/laravel_model_recommendation.php
@@ -2,6 +2,6 @@
 // Config for model recommendation
 // These are the default settings for models
 return [
-    'recommendation_count' = 5,
-    'recommendation_order' = 'asc'
+    'recommendation_count' => 5,
+    'recommendation_order' => 'asc'
 ];
