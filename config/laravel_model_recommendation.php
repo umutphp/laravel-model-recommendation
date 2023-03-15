da1fddddb4ab913bd6ea24798008d3d8aeff88b9
Khoshbin Ali Ahmed <xoshbin@gmail.com>
2023-03-15 22:48:52 +0300

    add uuid check for polymorphic tables in the config

--- config/laravel_model_recommendation.php
+++ config/laravel_model_recommendation.php
@@ -3,5 +3,6 @@
 // These are the default settings for models
 return [
     'recommendation_count' => 5,
-    'recommendation_order' => 'asc' // possible values asc, desc, random
+    'recommendation_order' => 'asc', // possible values asc, desc, random
+    'uuids' => false //Use uuid as primary key
 ];
