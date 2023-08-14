33c90f59d7da5a2ba5d56172c23bac7f29f88102
bretoreta <bremmurd08@gmail.com>
2023-08-15 02:08:21 +0300

    Optimize Database Query by Solving N+1 Query Problem When Loading Reccomendations

--- src/HasRecommendation.php
+++ src/HasRecommendation.php
@@ -300,7 +300,7 @@ trait HasRecommendation
      */
     public function getRecommendations($name)
     {
-        $config = self::getRecommendationConfig()[$name] ?? null;
+        $config = $this->getRecommendationConfig()[$name] ?? null;
 
         if ($config === null) {
             return [];
@@ -312,14 +312,8 @@ trait HasRecommendation
             ->where('source_id', $this->id)
             ->get();
 
-        $return = collect();
-
-        foreach ($recommendations as $recommendation) {
-            $model  = app($recommendation->target_type);
-            $target = $model->where('id', $recommendation->target_id)->first();
-
-            $return->push($target);
-        }
+        
+        $return = $this->whereIn('id', $recommendations->pluck('target_id'))->get();
 
         $order = $config['recommendation_order'] ?? config('laravel_model_recommendation.recommendation_count');
 
